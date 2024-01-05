1b9057cbde51e58c553b66984f0e0ed458b570d3
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-05 21:30:13 +0400

    Update umnojeniye.java

--- umnojeniye.java
+++ umnojeniye.java
@@ -1,9 +1,9 @@
-public class umnojeniye{
-public static void main (String[] arg){
-for (int i=1; i<=9; i++){
-for (int a=1; a<=9; a++)
-System.out.println(i +"*" +a + "=" +a*i);
+public class umnojeniye {
+  public static void main(String[] arg) {
+    for (int i = 1; i <= 9; i++) {
+      for (int a = 1; a <= 9; a++)
+        System.out.println(i + "*" + a + "=" + a * i);
 
-}
-}
+    }
+  }
 }
